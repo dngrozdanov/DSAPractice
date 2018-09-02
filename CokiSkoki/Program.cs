0d43253f9201ecd56f2dd2babdccab9328287028
eXtraGoZ <stranged96@gmail.com>
2018-09-03 00:25:04 +0300

    CokiSkoki Task (TBD)
    * To Be Done

---
+++ CokiSkoki/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CokiSkoki
+{
+    public static class Startup
+    {
+        private static void Main()
+        {
+            //Console.ReadLine();
+            int[] buildings = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] jumpsArray = new int[buildings.Length];
+
+            for (int i = 0; i < buildings.Length; i++)
+            {
+                for (int j = i + 1; j < buildings.Length; j++)
+                {
+                    if (buildings[i] < buildings[j])
+                        jumpsArray[i]++;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", jumpsArray));
+        }
+    }
+}
