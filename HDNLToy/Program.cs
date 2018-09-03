79c17c8f8cea7506b5a84aa20c438231a2711d4b
eXtraGoZ <stranged96@gmail.com>
2018-09-03 22:28:27 +0300

    Solving Problems - (TBD)

---
+++ HDNLToy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HDNLToy
+{
+    static class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            var stack = new Stack<string>();
+            for (int i = 0; i < num; i++)
+            {
+                stack.Push(Console.ReadLine());
+            }
+
+            foreach (var item in stack)
+            {
+                var current = int.Parse(item[2].ToString());
+
+                if (current)
+                {
+
+                }
+            }
+
+        }
+    }
+}
---
+++ StudentsOrder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentsOrder
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split();
+            var input = Console.ReadLine().Split();
+            var Passengers =
+                new LinkedList<string>();
+            var tempDictionary = new Dictionary<string, LinkedListNode<string>>();
+            foreach (var item in input)
+            {
+                var node = Passengers.AddLast(item);
+                tempDictionary.Add(item, node);
+            }
+            for (int i = 0; i < int.Parse(numbers[1]); i++)
+            {
+                var temp = Console.ReadLine().Split();
+                Passengers.Remove(temp[0]);
+                Passengers.AddBefore(tempDictionary[temp[1]], temp[0]);
+                tempDictionary[temp[0]] = Passengers.AddBefore(tempDictionary[temp[1]], temp[0]);
+            }
+            foreach (var item in Passengers)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
