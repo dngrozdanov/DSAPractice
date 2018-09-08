c4b365fa8506b9cf2b8c7849c067927458f26236
eXtraGoZ <stranged96@gmail.com>
2018-09-08 19:49:45 +0300

    CokiSkoki

--- CokiSkoki/Program.cs
+++ CokiSkoki/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,20 +8,54 @@ namespace CokiSkoki
     {
         private static void Main()
         {
-            //Console.ReadLine();
-            int[] buildings = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int[] jumpsArray = new int[buildings.Length];
+            Console.ReadLine();
+            var buildingsRaw = Console.ReadLine().Split();
+            var buildings = new int[buildingsRaw.Length];
+            // Cycle it manually to reduce processing time.
+            for (int i = 0; i < buildingsRaw.Length; i++)
+            {   
+                buildings[i] = int.Parse(buildingsRaw[i]);
+            }
+            var jumpsArray = new int[buildings.Length];
 
-            for (int i = 0; i < buildings.Length; i++)
+            // Use RAM instead of cycling again thru the array to get the MAX value
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = buildings.Length - 1; i >= 0; i--)
             {
-                for (int j = i + 1; j < buildings.Length; j++)
+                while (stack.Count > 0 && buildings[stack.Peek()] <= buildings[i])
                 {
-                    if (buildings[i] < buildings[j])
-                        jumpsArray[i]++;
+                    int peekIndex = stack.Pop();
+                    jumpsArray[peekIndex] = stack.Count;
                 }
+
+                stack.Push(i);
             }
 
-            Console.WriteLine(string.Join(" ", jumpsArray));
+            while (stack.Count > 0)
+            {
+                int peekIndex = stack.Pop();
+                jumpsArray[peekIndex] = stack.Count;
+            }
+            #region Old Solution
+            /*int maxHigh = 0;
+            for (var i = 0; i < buildings.Length; i++)
+            {
+                int lastHighestHeight = buildings[i], count = 0;
+                for (var j = i + 1; j < buildings.Length; j++)
+                    if (lastHighestHeight < buildings[j])
+                    {
+                        lastHighestHeight = buildings[j];
+                        count++;
+                    }
+
+                jumpsArray[i] = count;
+                if (count > maxHigh)
+                    maxHigh = count;
+            }*/
+            #endregion
+            
+            Console.WriteLine(jumpsArray.Max() + Environment.NewLine + string.Join(" ", jumpsArray));
         }
     }
 }
---
+++ MergeBinaryTrees/Startup.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeBinaryTrees
+{
+    internal static class Startup
+    {
+        private static void Main()
+        {
+            #region INFO: Binary Search Tree & Merge Binary Trees
+            // INFO: Binary Search Tree & Merge Binary Trees
+            /*var firstTree = new Solution.TreeNode(1)
+            {
+                left = new Solution.TreeNode(3) {left = new Solution.TreeNode(5)}
+            };
+            firstTree.right = new Solution.TreeNode(2);
+
+            var secondThree = new Solution.TreeNode(2)
+            {
+                left = new Solution.TreeNode(1), right = new Solution.TreeNode(3)
+            };
+            secondThree.left.right = new Solution.TreeNode(4);
+            secondThree.right.right = new Solution.TreeNode(7);
+
+            var mergedTreeRoot = Solution.MergeTrees(firstTree, secondThree);
+
+            var foundResult = Solution.SearchBST(mergedTreeRoot, 7);*/
+
+            /*var firstTree = new Solution.TreeNode(4);
+            Solution.InsertData(ref firstTree, 2);
+            Solution.InsertData(ref firstTree, 1);
+            Solution.InsertData(ref firstTree, 3);
+            Solution.InsertData(ref firstTree, 7);
+
+            var foundResultBST = Solution.SearchBST(firstTree, 5);
+            Console.WriteLine(foundResultBST);*/
+            #endregion
+            
+            #region All Possible Paths Logic
+            // INFO: resultTree - All Possible Paths
+            var firstTree = new Solution.TreeNode(4);
+            // Test Cases
+            var Data = new List<int>
+                {5, 10, 1, 2, 3, 6, 7, 11, 20, 12, 8, 44, 24, 17, 21, 77, 88, 99, 34, 45, 67, 41, 32, 38};
+            Solution.InsertData(ref firstTree, Data);
+
+            var resultTree = Solution.BinaryTreePaths(firstTree);
+            foreach (var tree in resultTree) Console.WriteLine(@"Possible Path: " + tree);
+            #endregion
+        }
+    }
+
+    internal static class Solution
+    {
+        public static TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null)
+                return t2;
+            if (t2 == null)
+                return t1;
+            t1.val += t2.val;
+            t1.left = MergeTrees(t1.left, t2.left);
+            t1.right = MergeTrees(t1.right, t2.right);
+            return t1;
+        }
+
+        public static TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+                return null;
+            if (root.val == val)
+                return root;
+            if (val < root.val && root.left != null)
+                return SearchBST(root.left, val);
+            if (val > root.val && root.right != null)
+                return SearchBST(root.right, val);
+
+            return null;
+        }
+
+        public static bool IsLeaf(TreeNode node)
+        {
+            return node.right == null && node.left == null;
+        }
+
+        private static void DFSHelper(TreeNode node, ref List<string> result, string partial)
+        {
+            if (node.left == null && node.right == null) result.Add(partial + node.val);
+
+            if (node.left != null)
+                DFSHelper(node.left, ref result, partial + node.val + " -> ");
+            if (node.right != null)
+                DFSHelper(node.right, ref result, partial + node.val + " -> ");
+        }
+
+        /// <summary>
+        ///     Recursive DFS (Depth-First Search) [Pre-Order]
+        /// </summary>
+        /// <param name="root">TreeNode</param>
+        public static IEnumerable<string> BinaryTreePaths(TreeNode root)
+        {
+            var result = new List<string>();
+            if (root == null)
+                return result;
+            DFSHelper(root, ref result, "");
+            return result;
+
+            #region Old Methods
+
+            /*if (!root.isParent)
+            {
+                Console.WriteLine(0);
+                binaryTreePathsList.Add(0);
+            }
+            else
+            {
+                Console.WriteLine(root.val);
+                binaryTreePathsList.Add(root.val);
+            }
+
+            if (root.left != null)
+            {
+                BinaryTreePaths(root.left);
+            }
+            if (root.right != null)
+            {
+                BinaryTreePaths(root.right);
+            }*/
+
+
+            /*Stack<TreeNodeChildren> s = new Stack<TreeNodeChildren>();
+            s.Push(root);
+            while (s.Count > 0)
+            {
+                var n = s.Pop();
+                // Do Action
+                Console.WriteLine(n.val);
+                binaryTreePathsList.Add(n.val);
+                foreach (var child in n.children.ToArray().Reverse())
+                {
+                    s.Push(child);
+                    
+                }
+            }*/
+            // Do action
+            //Console.WriteLine(root.val);
+            //binaryTreePathsList.Add(root.val);
+
+            #endregion
+        }
+
+        public static void InsertData(ref TreeNode node, int data)
+        {
+            if (node == null)
+                node = new TreeNode(data);
+            else if (node.val < data)
+                InsertData(ref node.right, data);
+
+            else if (node.val > data) InsertData(ref node.left, data);
+        }
+
+        public static void InsertData(ref TreeNode node, IEnumerable<int> dataList)
+        {
+            foreach (var data in dataList)
+            {
+                if (node == null)
+                    node = new TreeNode(data);
+                else if (node.val < data)
+                    InsertData(ref node.right, data);
+
+                else if (node.val > data) InsertData(ref node.left, data);
+            }
+        }
+
+        public class TreeNode
+        {
+            public TreeNode left;
+            public TreeNode right;
+            public int val;
+
+            public TreeNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
