434a3c41e544c4660686e4511ab0a085a8251bf0
eXtraGoZ <stranged96@gmail.com>
2018-09-11 13:13:18 +0300

    Yesterday's Contest

---
+++ Expressions/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Expressions
+{
+    class Solution
+    {
+        public static List<String> answer;
+
+        public static String digits;
+
+        public static long target;
+
+        static void Main()
+        {
+            AddOperators(Console.ReadLine(), int.Parse(Console.ReadLine()));
+            Console.WriteLine(answer.Count);
+        }
+
+        public static void Recurse(int index, long value, StringBuilder ops, long previousValue)
+        {
+            String nums = digits;
+
+            if ((index == nums.Length))
+            {
+                if ((value == target))
+                {
+                    answer.Add(ops.ToString());
+                }
+
+                return;
+            }
+
+            long current_val = 0;
+            String current_val_rep = null;
+            int length = nums.Length;
+
+            for (int i = index; (i < length); i++)
+            {
+
+                current_val = (long)((current_val * 10)
+                               + Char.GetNumericValue(nums[i]));
+                current_val_rep = current_val.ToString();
+
+                if ((index == 0))
+                {
+                    Recurse((i + 1), current_val, new StringBuilder(ops.ToString() + (current_val_rep)), current_val);
+                }
+                else
+                {
+                    long v = (value - previousValue);
+
+                    Recurse((i + 1), (v
+                                    + (previousValue * current_val)), new StringBuilder(ops.ToString() + '*' + (current_val_rep)), (previousValue * current_val));
+
+                    Recurse((i + 1), (value + current_val), new StringBuilder(ops.ToString() + '+' + current_val_rep), current_val);
+
+                    Recurse((i + 1), (value - current_val), new StringBuilder(ops.ToString() + '-' + current_val_rep), (current_val * -1));
+                }
+
+                if ((nums[index] == '0'))
+                {
+                    break;
+                }
+            }
+        }
+
+        public static List<String> AddOperators(String num, int tempTarget)
+        {
+            if ((num.Length == 0))
+            {
+                return new List<String>();
+            }
+
+            target = tempTarget;
+            digits = num;
+            answer = new List<String>();
+            Recurse(0, 0, new StringBuilder(), 0);
+            return answer;
+        }
+    }
+}
---
+++ GreaterMoney/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GreaterMoney
+{
+    class Program
+    {
+        static void Main()
+        {
+            var firstBag = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+            var secondBag = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+            var resultBag = new List<int>();
+            for (int i = 0; i < firstBag.Count; i++)
+            {
+                for (int y = secondBag.IndexOf(firstBag[i]); y < secondBag.Count; y++)
+                {
+                    if (firstBag[i] < secondBag[y])
+                    {
+                        resultBag.Add(secondBag[y]);
+                        break;
+                    }
+                    if (y == secondBag.Count - 1)
+                    {
+                        resultBag.Add(-1);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(",", resultBag));
+        }
+    }
+}
